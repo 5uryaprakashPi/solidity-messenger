f8a900dad42c27c8a85088a80141463a36019853
5uryaprakashPi <134121887+5uryaprakashPi@users.noreply.github.com>
2023-06-17 11:49:32 +0530

    added registration and event on new message

--- Messenger.sol
+++ Messenger.sol
@@ -1,39 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "hardhat/console.sol";
-
 struct Message {
-    address from;
+    string from;
     string message;
 }
 
 contract Messenger {
+    //name to address
+    mapping(string => address) private nameToAddress;
+    //name to address
+    mapping(address => string) private addressToName;
+    //check address registered
+    mapping(address => bool) private addressToBool;
+    //address to messages mapping
+    mapping(address => Message[]) private userMessages;
 
-    Message public message;
-    mapping(string=>address) private registry;
-    mapping(address=>Message[]) private userMessages;
+    event newMessage(address indexed _receiver, Message _message);
 
-    function send(string calldata name, string calldata data)
-    public returns(bool)  {
-        console.log(1);
-        Message memory _message = Message(msg.sender, data);
-
-        console.log(2);
-        userMessages[registry[name]].push(_message);
-
-        console.log(3);
+    function send(
+        string calldata to,
+        string calldata data
+    ) public returns (bool) {
+        Message memory _message = Message(addressToName[msg.sender], data);
+        userMessages[nameToAddress[to]].push(_message);
+        emit newMessage(nameToAddress[to], _message);
         return true;
     }
 
-    function register(string calldata name) 
-    public {
-        registry[name] = msg.sender;
+    function register(string calldata name) public {
+        //require(name);
+        nameToAddress[name] = msg.sender;
+        addressToBool[msg.sender] = true;
+        addressToName[msg.sender] = name;
     }
 
-    function getMessages()
-    public view returns (Message[] memory messages) {
+    function getMessages() public view returns (Message[] memory) {
         return userMessages[msg.sender];
     }
 
+    function isRegistered() public view returns (string memory) {
+        if (addressToBool[msg.sender]) {
+            return addressToName[msg.sender];
+        }
+        return "NONE";
+    }
 }
